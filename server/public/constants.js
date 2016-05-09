60f5c7bcc12ffeea709e30ef58f250ee62353839
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-05-09 23:56:54 +0200

    sort constant name

--- server/public/constants.js
+++ server/public/constants.js
@@ -3,35 +3,31 @@ var Constants = (function() {
   var Constants = function() {
   };
 
-  Constants.prototype.connection = "connection";
+  Constants.prototype.acceleration = "acceleration";
+  Constants.prototype.closeCo = "closeCo";
   Constants.prototype.closeConnection = "closeConnection";
-  Constants.prototype.endGame = "endGame";
+  Constants.prototype.connection = "connection";
   Constants.prototype.counting = "counting";
-  Constants.prototype.startGame = "startGame";
+  Constants.prototype.disconnect = "disconnect";
+  Constants.prototype.disconnection = "disconnection";
+  Constants.prototype.endGame = "endGame";
+  Constants.prototype.error = "error";
+  Constants.prototype.gameDisconnect = "gameDisconnect";
+  Constants.prototype.id = "id";
+  Constants.prototype.infoPart = "infoPart";
+  Constants.prototype.instanceDisconnection = "instanceDisconnection";
+  Constants.prototype.isExist = "isExist";
   Constants.prototype.login = "login";
   Constants.prototype.logins = "logins";
-  Constants.prototype.ping = "ping";
-  Constants.prototype.trackPosition = "trackPosition";
-  Constants.prototype.gameDisconnect = "gameDisconnect";
-  Constants.prototype.myPosition = "myPosition";
-  Constants.prototype.position = "position";
-  Constants.prototype.isExist = "isExist";
-  Constants.prototype.closeCo = "closeCo";
-  Constants.prototype.disconnection = "disconnection";
-  Constants.prototype.instanceDisconnection = "instanceDisconnection";
-  Constants.prototype.infoPart = "infoPart";
-  Constants.prototype.id = "id";
-  Constants.prototype.error = "error";
-  Constants.prototype.disconnect = "disconnect";
-
-  Constants.prototype.acceleration = "acceleration";
-
-  Constants.prototype.switchTrack = "switchTrack";
-
   Constants.prototype.message = "message";
+  Constants.prototype.myPosition = "myPosition";
   Constants.prototype.oldMessages = "oldMessages";
-
+  Constants.prototype.ping = "ping";
+  Constants.prototype.position = "position";
   Constants.prototype.serverMessage = "serverMessage";
+  Constants.prototype.startGame = "startGame";
+  Constants.prototype.switchTrack = "switchTrack";
+  Constants.prototype.trackPosition = "trackPosition";
 
   return Constants;
 })();
