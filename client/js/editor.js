6b737d50db4d2763270095c346b7ebd2ee3859ee
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-05-12 22:20:09 +0200

    code refactoring

--- client/js/editor.js
+++ client/js/editor.js
@@ -1,96 +1,88 @@
 function drawListImage ()
 {
-	var path = 'assets/';
-	var insert = '';
-
-	// 	</canvas>
-	// 	<canvas id="myCanvas1" width="30" height="30">
-	// 	<canvas id="myCanvas2" width="30" height="30"></canvas>
-	// for ( var i = 0 ; i < m_listImgURL.length ; i++ )
-	// {
-	// 	var url = path + m_listImgURL[i] ;
-	// 	insert += '<canvas id="myCanvas'+i+'" width="30" height="30">';
-	// 	insert += '<img class="img" src="'+url+'" id="'+i+'" onClick="(function(){m_level.setIndice('+i+');m_level.drawImageCurrent();}());">';
-	// 	insert += '</canvas>';
-	// }
-	// insert += '';
-	// liste_image.innerHTML = insert;
-	for ( var i = 0 ; i < m_listImgURL.length ; i++ )
-	{
-		insert += '<canvas id="myCanvas'+i+'" width="30" height="30" onclick="(function(){m_level.setIndice('+i+');m_level.drawImageCurrent();}());">';
-		insert += '</canvas>';
-	}
-	liste_image.innerHTML = insert;
-	for ( var i = 0 ; i < m_listImgURL.length ; i++ )
-	{
-		var url = path + m_listImgURL[i] ;
-		var c = document.getElementById("myCanvas"+i);
-		var ctx = c.getContext("2d");
-		var img = document.createElement("IMG");
-		ctx.clearRect ( 0 , 0 , c.width , c.height );
-		ctx.translate(c.width/2, c.height/2);
-		img.src=url;
-		img.onclick="(function(){m_level.setIndice('+i+');m_level.drawImageCurrent();}());";
-
-		ctx.rotate(Math.PI*(document.getElementById('rotate').value)/180);
-		// console.log(document.getElementById('rotate').value-oldAngle);
-		ctx.translate(-c.width/2, -c.height/2);
-		ctx.drawImage(img,0,0);
-	}
-	oldAngle = document.getElementById('rotate').value;
+  var path = 'assets/';
+  var insert = '';
+  for ( var i = 0 ; i < m_listImgURL.length ; i++ )
+  {
+    insert += '<canvas id="myCanvas'+i+'" width="30" height="30" onclick="(function(){m_level.setIndice('+i+');m_level.drawImageCurrent();}());">';
+    insert += '</canvas>';
+  }
+  liste_image.innerHTML = insert;
+  var onClickFunction = "(function(){m_level.setIndice('+i+');m_level.drawImageCurrent();}());";
+  for ( var i = 0 ; i < m_listImgURL.length ; i++ )
+  {
+      drawImageByContext(path + m_listImgURL[i], "myCanvas"+i, onClickFunction);
+  }
+  oldAngle = document.getElementById('rotate').value;
 }
-
-function Editor ( )
+  
+function drawImageByContext(url, id , onclick = null) 
 {
-	var m_viewport;
-	this.m_level;
-	var m_background;
-	this.copyLevel;
-	var cameraSpeed = 5;
-	var cell_size = 50;
-	background = new jaws.Sprite({image : ''});
+  var c = document.getElementById(id);
+  var ctx = c.getContext("2d");
+  var img = document.createElement("IMG");
+  ctx.clearRect ( 0 , 0 , c.width , c.height );
+  ctx.translate(c.width/2, c.height/2);
+  img.src = url;
+  img.onclick = onclick;
 
-	this.setup = function ()
-	{
-		live_info = document.getElementById("live_info");
-		liste_image = document.getElementById("liste-image");
-		current_cursor_image = document.getElementById('current-cursor-image');
-
-		//Viewport
-		m_viewport = new jaws.Viewport({max_x: jaws.width*1.5, max_y: jaws.height*1.5});
-
-		m_level = new MakeLevel ( cell_size , m_listImgURL , m_viewport);
-		m_level.constructor();
-
-		drawListImage();
-
-		jaws.preventDefaultKeys(["up", "down", "left", "right", "space"]);
-	}
-
-	this.update = function ()
-	{
-		moveCamera();
-		m_level.update(m_viewport);
-
-		//Infos
-		live_info.innerHTML = jaws.game_loop.fps + " fps. X: " + parseInt(jaws.mouse_x) + "/ Y : " + parseInt(jaws.mouse_y) + ". ";
-    live_info.innerHTML += "Viewport: " + parseInt(m_viewport.x) + "/" + parseInt(m_viewport.y) + ".";
-
-		moveScreen();
-		mouseFollow();
-	}
-
-	function test(i)
-	{
-		m_indiceIMG = i;
-	}
-
-	// function drawListImage ()
-	// {
-	// 	var path = 'assets/';
-	// 	var insert = '';
-
-	// 	// 	</canvas>
+  ctx.rotate(Math.PI*(document.getElementById('rotate').value)/180);
+  // console.log(document.getElementById('rotate').value-oldAngle);
+  ctx.translate(-c.width/2, -c.height/2);
+  ctx.drawImage(img,0,0);
+}
+  
+  function Editor ( )
+  {
+  var m_viewport;
+  this.m_level;
+  var m_background;
+  this.copyLevel;
+  var cameraSpeed = 5;
+  var cell_size = 50;
+  background = new jaws.Sprite({image : ''});
+  
+  this.setup = function ()
+  {
+    live_info = document.getElementById("live_info");
+    liste_image = document.getElementById("liste-image");
+    current_cursor_image = document.getElementById('current-cursor-image');
+  
+    //Viewport
+    m_viewport = new jaws.Viewport({max_x: jaws.width*1.5, max_y: jaws.height*1.5});
+  
+    m_level = new MakeLevel ( cell_size , m_listImgURL , m_viewport);
+    m_level.constructor();
+  
+    drawListImage();
+  
+    jaws.preventDefaultKeys(["up", "down", "left", "right", "space"]);
+  }
+  
+  this.update = function ()
+  {
+    moveCamera();
+    m_level.update(m_viewport);
+  
+    //Infos
+    live_info.innerHTML = jaws.game_loop.fps + " fps. X: " + parseInt(jaws.mouse_x) + "/ Y : " + parseInt(jaws.mouse_y) + ". ";
+      live_info.innerHTML += "Viewport: " + parseInt(m_viewport.x) + "/" + parseInt(m_viewport.y) + ".";
+  
+    moveScreen();
+    mouseFollow();
+  }
+  
+  function test(i)
+  {
+    m_indiceIMG = i;
+  }
+  
+  // function drawListImage ()
+  // {
+  //   var path = 'assets/';
+  //   var insert = '';
+  
+  //   //
 	// 	// 	<canvas id="myCanvas1" width="30" height="30">
 	// 	// 	<canvas id="myCanvas2" width="30" height="30"></canvas>
 	// 	// for ( var i = 0 ; i < m_listImgURL.length ; i++ )
