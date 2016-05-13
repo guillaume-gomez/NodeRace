50266577537c94a5b130ac337a94dc4c64d7e30f
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-05-13 11:29:21 +0200

    code formatted using HTML-CSS-JS Prettify

--- server/public/constants.js
+++ server/public/constants.js
@@ -1,35 +1,34 @@
 // constants.js
 var Constants = (function() {
-  var Constants = function() {
-  };
+    var Constants = function() {};
 
-  Constants.prototype.acceleration = "acceleration";
-  Constants.prototype.closeCo = "closeCo";
-  Constants.prototype.closeConnection = "closeConnection";
-  Constants.prototype.connection = "connection";
-  Constants.prototype.counting = "counting";
-  Constants.prototype.disconnect = "disconnect";
-  Constants.prototype.disconnection = "disconnection";
-  Constants.prototype.endGame = "endGame";
-  Constants.prototype.error = "error";
-  Constants.prototype.gameDisconnect = "gameDisconnect";
-  Constants.prototype.id = "id";
-  Constants.prototype.infoPart = "infoPart";
-  Constants.prototype.instanceDisconnection = "instanceDisconnection";
-  Constants.prototype.isExist = "isExist";
-  Constants.prototype.login = "login";
-  Constants.prototype.logins = "logins";
-  Constants.prototype.message = "message";
-  Constants.prototype.myPosition = "myPosition";
-  Constants.prototype.oldMessages = "oldMessages";
-  Constants.prototype.ping = "ping";
-  Constants.prototype.position = "position";
-  Constants.prototype.serverMessage = "serverMessage";
-  Constants.prototype.startGame = "startGame";
-  Constants.prototype.switchTrack = "switchTrack";
-  Constants.prototype.trackPosition = "trackPosition";
+    Constants.prototype.acceleration = "acceleration";
+    Constants.prototype.closeCo = "closeCo";
+    Constants.prototype.closeConnection = "closeConnection";
+    Constants.prototype.connection = "connection";
+    Constants.prototype.counting = "counting";
+    Constants.prototype.disconnect = "disconnect";
+    Constants.prototype.disconnection = "disconnection";
+    Constants.prototype.endGame = "endGame";
+    Constants.prototype.error = "error";
+    Constants.prototype.gameDisconnect = "gameDisconnect";
+    Constants.prototype.id = "id";
+    Constants.prototype.infoPart = "infoPart";
+    Constants.prototype.instanceDisconnection = "instanceDisconnection";
+    Constants.prototype.isExist = "isExist";
+    Constants.prototype.login = "login";
+    Constants.prototype.logins = "logins";
+    Constants.prototype.message = "message";
+    Constants.prototype.myPosition = "myPosition";
+    Constants.prototype.oldMessages = "oldMessages";
+    Constants.prototype.ping = "ping";
+    Constants.prototype.position = "position";
+    Constants.prototype.serverMessage = "serverMessage";
+    Constants.prototype.startGame = "startGame";
+    Constants.prototype.switchTrack = "switchTrack";
+    Constants.prototype.trackPosition = "trackPosition";
 
-  return Constants;
+    return Constants;
 })();
 
 module.exports = Constants;
