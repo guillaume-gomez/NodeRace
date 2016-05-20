9a1c8eb5fb0a8e60f9a9b5fa42cbeb036151afb0
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-05-21 00:38:44 +0200

    fix the chat submit button and improve view

--- client/js/manageChat.js
+++ client/js/manageChat.js
@@ -2,9 +2,8 @@ function manageChat(socket) {
     socket.on(jaws.constants.message, function(message) {
         var messages = document.getElementById("listMessage");
         var messageJson = JSON.parse(message);
-        messages.innerHTML += "<p>" + messageJson.login + " : " + messageJson.message + "</p>";
+        messages.innerHTML += "<p class='text-muted'>" + messageJson.login + " : " + messageJson.message + "</p>";
     })
-
     socket.on(jaws.constants.oldMessages, function(message) {
         var oldMessages = document.getElementById("oldMessages");
         var messagesJson = JSON.parse(message);
@@ -12,10 +11,10 @@ function manageChat(socket) {
         messagesJson.forEach(logArrayElements);
     })
 
-    $('#submit').click(function() {
+    $('#submitChatMessage').click(function() {
         var message = document.getElementById("message").value;
         var listMessage = document.getElementById("listMessage");
-        listMessage.innerHTML += "<p>Moi : " + message + "</p>";
+        listMessage.innerHTML += "<p class='text-muted'>Moi : " + message + "</p>";
         console.log(message);
         socket.emit(jaws.constants.message, message);
         return false;
@@ -23,6 +22,6 @@ function manageChat(socket) {
 
     function logArrayElements(element, index, array) {
         var messages = document.getElementById("listMessage");
-        messages.innerHTML += "<p>" + element.login + " : " + element.message + "</p>";
+        messages.innerHTML += "<p class='text-muted'>" + element.login + " : " + element.message + "</p>";
     }
 }
