6a869b91d3ffdd33fec4459ba2fe5808d8ca73e4
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-05-22 17:30:40 +0200

    start smartphone controls

---
+++ client/js/smartphone-controls.js
@@ -0,0 +1,8 @@
+function startup() {
+  var el = document.getElementsByTagName("gameCanvas")[0];
+  el.addEventListener("touchstart", handleStart, false);
+  el.addEventListener("touchend", handleEnd, false);
+  el.addEventListener("touchcancel", handleCancel, false);
+  el.addEventListener("touchmove", handleMove, false);
+  log("initialized.");
+}
