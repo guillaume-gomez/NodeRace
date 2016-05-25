e3dbca1f0752bdd6cb9eeb2a24c81c7976116f6f
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-05-25 22:14:18 +0200

    add class tools

---
+++ server/js/interval.js
@@ -0,0 +1,22 @@
+export.Interval = function(fn, time) {
+    var timer = false;
+    this.start = function () {
+        if (!this.isRunning())
+            timer = setInterval(fn, time);
+    };
+    this.stop = function () {
+        clearInterval(timer);
+        timer = false;
+    };
+    this.isRunning = function () {
+        return timer !== false;
+    };
+}
+
+var i = new Interval(fncName, 1000);
+i.start();
+
+if (i.isRunning())
+    // ...
+
+i.stop();
