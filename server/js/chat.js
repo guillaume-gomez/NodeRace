e791e2e602f5b2f59a533280fa1a56b51fafe3a9
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-06-04 00:29:56 +0200

    code refactoring

--- server/js/chat.js
+++ server/js/chat.js
@@ -5,27 +5,14 @@ var ArrayMessages = new Array();
 
 exports.addNewMessage = function(socket) {
     // trigger on 'send' button
-    socket.on(constants.message, function(message) {
-        // get the nickname
-        var newMessage = {
-            'login': socket.login,
-            'message': message
-        };
-        for (var i = 0; i < ArrayMessages.length; i++) {
-            if (socket.uid == ArrayMessages[i].id) {
-                ArrayMessages[i].listMessage.push(newMessage);
-                socket.broadcast.to(ArrayMessages[i].room).emit(constants.message, JSON.stringify(newMessage));
-
-                if (ArrayMessages[i].listMessage.length > 15) {
-                    //delete the oldest messages
-                    ArrayMessages[i].listMessage.splice(0, 1);
-                }
-                break;
-            }
-        }
-    });
+    var fnc = performNewMessage.bind(socket);
+    socket.on(constants.message, fnc);
 }
 
+exports.notifyNewPlayer = function(socket) {
+    var message = "'socket.login' " + constants.notifyNewPlayerMessage;
+    performNewMessage(message)(socket);
+}
 
 exports.getOldMessages = function(socket) {
     for (var i = 0; i < ArrayMessages.length; i++) {
@@ -57,4 +44,28 @@ exports.deleteChatInstance = function(id) {
             break;
         }
     }
+}
+
+function performNewMessage(message) {
+    var socketVar = this;
+    // get the nickname
+    var newMessage = {
+        'login': socketVar.login,
+        'message': message
+    };
+    var messages = getArrayMessage(socketVar.uid);
+    socketVar.broadcast.to(messages.room).emit(constants.message, JSON.stringify(newMessage));
+    if (messages.listMessage.length > 15) {
+        //delete the oldest messages
+        messages.listMessage.splice(0, 1);
+    }
+}
+
+function getArrayMessage(socketUid) {
+    for (var i = 0; i < ArrayMessages.length; i++) {
+         if (socketUid == ArrayMessages[i].id) {
+            return ArrayMessages[i];
+         }
+    }
+    return null;
 }
