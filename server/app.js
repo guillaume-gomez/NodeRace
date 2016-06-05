8a2a9c0594882fd1ed29e566bab3a58001ae8182
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-06-05 11:42:32 +0200

    notify player that the host leaved the game

--- server/app.js
+++ server/app.js
@@ -39,6 +39,7 @@ function tick(socket, carInfos) {
             console.log("{ " + socket.login + "}:  disconnection : ping too high   " + socket.id);
             car = tools.findCar(instances[socket.uid], socket.id);
             if (car != -1 && car.isHost) {
+                tools.hostIsLeaving(socket, instances[socket.uid]);
                 tools.disconnectEveryone(socket, instances[socket.uid]);
                 tools.disconnect(socket, instances, chatF);
                 return;
@@ -232,6 +233,7 @@ io.on(constants.connection, function(socket) {
             //get info from the recipient
             car = tools.findCar(instances[socket.uid], socket.id);
             if (car != -1 && car.isHost) {
+                tools.hostIsLeaving(socket, instances[socket.uid]);
                 tools.disconnectEveryone(socket, instances[socket.uid]);
                 tools.disconnect(socket, instances, chatF);
                 return;
